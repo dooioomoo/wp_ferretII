af0a8fdec2cfa73228aa1766052be4e304ccda08
dooioomoo <dooioomoo@gmail.com>
2020-03-13 21:33:55 +0900

    Update template-customize.php

--- inc/template/template-customize.php
+++ inc/template/template-customize.php
@@ -1,39 +1,39 @@
 <?php
     /**
-     * nishifunabashihotel's template customize
-     * @package nishifunabashihotel
+     * _ferret's template customize
+     * @package _ferret
      */
     
-    add_action('customize_register', 'nishifunabashihotel_template_customize_register');
-    add_action('customize_register', 'nishifunabashihotel_logo_uploader');
-    add_filter('comment_form_defaults', 'nishifunabashihotel_comment_form_change');
+    add_action('customize_register', '_ferret_template_customize_register');
+    add_action('customize_register', '_ferret_logo_uploader');
+    add_filter('comment_form_defaults', '_ferret_comment_form_change');
     
-    if (!function_exists('nishifunabashihotel_template_customize_register')):
-        function nishifunabashihotel_template_customize_register($wp_customize) {
+    if (!function_exists('_ferret_template_customize_register')):
+        function _ferret_template_customize_register($wp_customize) {
             /**
              * add default view for widget
              */
             
-            $wp_customize->add_panel('nishifunabashihotel_theme_options', array (
+            $wp_customize->add_panel('_ferret_theme_options', array (
                 'priority'       => 500,
                 'theme_supports' => '',
-                'title'          => __('Theme Options', 'nishifunabashihotel'),
-                'description'    => __('set something for the theme.', 'nishifunabashihotel'),
+                'title'          => __('Theme Options', '_ferret'),
+                'description'    => __('set something for the theme.', '_ferret'),
             ));
             
-            $wp_customize->add_section('nishifunabashihotel_theme_options_widget_section', array (
-                'title'       => __('Widget', 'nishifunabashihotel'),
+            $wp_customize->add_section('_ferret_theme_options_widget_section', array (
+                'title'       => __('Widget', '_ferret'),
                 'description' => __('custom theme widget options in here'),
-                'panel'       => 'nishifunabashihotel_theme_options', // Not typically needed.
+                'panel'       => '_ferret_theme_options', // Not typically needed.
             ));
             
-            $wp_customize->add_section('nishifunabashihotel_theme_options_loader_section', array (
-                'title'       => __('Loader', 'nishifunabashihotel'),
+            $wp_customize->add_section('_ferret_theme_options_loader_section', array (
+                'title'       => __('Loader', '_ferret'),
                 'description' => __('set your page loader in here'),
-                'panel'       => 'nishifunabashihotel_theme_options', // Not typically needed.
+                'panel'       => '_ferret_theme_options', // Not typically needed.
             ));
             
-            nishifunabashihotel_custom_default_view_by_posttype($wp_customize);
+            _ferret_custom_default_view_by_posttype($wp_customize);
             
         }
     endif;
@@ -43,24 +43,24 @@
      *
      * @param $wp_customize
      */
-    function nishifunabashihotel_custom_default_view_by_posttype($wp_customize) {
+    function _ferret_custom_default_view_by_posttype($wp_customize) {
         
         /***
          * WIDGET
          */
         $wp_customize->add_setting(
-            'nishifunabashihotel_widget_default_order_master', array (
+            '_ferret_widget_default_order_master', array (
                 'default'   => 'right',
                 'transport' => 'postMessage',
             )
         );
         $wp_customize->add_control(new WP_Customize_Control(
                 $wp_customize,
-                'nishifunabashihotel_widget_default_order_master',
+                '_ferret_widget_default_order_master',
                 array (
-                    'label'    => __('Default sidebar for master', 'nishifunabashihotel'),
-                    'section'  => 'nishifunabashihotel_theme_options_widget_section',
-                    'settings' => 'nishifunabashihotel_widget_default_order_master',
+                    'label'    => __('Default sidebar for master', '_ferret'),
+                    'section'  => '_ferret_theme_options_widget_section',
+                    'settings' => '_ferret_widget_default_order_master',
                     'type'     => 'select',
                     'choices'  => array (
                         'right' => __('right'),
@@ -69,38 +69,38 @@
                 )
             )
         );
-        $post_type = nishifunabashihotel_get_all_posttype();
+        $post_type = _ferret_get_all_posttype();
         foreach ($post_type as $index => $type) {
             $wp_customize->add_setting(
-                'nishifunabashihotel_widget_default_view_' . $type, array (
+                '_ferret_widget_default_view_' . $type, array (
                     'default'   => 'none',
                     'transport' => 'postMessage',
                 )
             );
             $wp_customize->add_control(new WP_Customize_Control(
                     $wp_customize,
-                    'nishifunabashihotel_widget_default_view_' . $type,
+                    '_ferret_widget_default_view_' . $type,
                     array (
-                        'label'    => __('Default sidebar for', 'nishifunabashihotel') . ' \'' . $type . '\'',
-                        'section'  => 'nishifunabashihotel_theme_options_widget_section',
-                        'settings' => 'nishifunabashihotel_widget_default_view_' . $type,
+                        'label'    => __('Default sidebar for', '_ferret') . ' \'' . $type . '\'',
+                        'section'  => '_ferret_theme_options_widget_section',
+                        'settings' => '_ferret_widget_default_view_' . $type,
                         'type'     => 'select',
-                        'choices'  => nishifunabashihotel_get_all_sidebar()
+                        'choices'  => _ferret_get_all_sidebar()
                     )
                 )
             );
             $wp_customize->add_setting(
-                'nishifunabashihotel_widget_default_order_' . $type, array (
+                '_ferret_widget_default_order_' . $type, array (
                     'default'   => 'right',
                     'transport' => 'postMessage',
                 )
             );
             $wp_customize->add_control(new WP_Customize_Control(
                     $wp_customize,
-                    'nishifunabashihotel_widget_default_order_' . $type,
+                    '_ferret_widget_default_order_' . $type,
                     array (
-                        'section'  => 'nishifunabashihotel_theme_options_widget_section',
-                        'settings' => 'nishifunabashihotel_widget_default_order_' . $type,
+                        'section'  => '_ferret_theme_options_widget_section',
+                        'settings' => '_ferret_widget_default_order_' . $type,
                         'type'     => 'select',
                         'choices'  => array (
                             'right' => __('right'),
@@ -117,18 +117,18 @@
          */
         
         $wp_customize->add_setting(
-            'nishifunabashihotel_theme_options_loader_settings', array (
+            '_ferret_theme_options_loader_settings', array (
                 'default'   => 'none',
                 'transport' => 'postMessage',
             )
         );
         $wp_customize->add_control(new WP_Customize_Control(
                 $wp_customize,
-                'nishifunabashihotel_loader_default_order_master',
+                '_ferret_loader_default_order_master',
                 array (
-                    'label'    => __('add a loader element', 'nishifunabashihotel'),
-                    'section'  => 'nishifunabashihotel_theme_options_loader_section',
-                    'settings' => 'nishifunabashihotel_theme_options_loader_settings',
+                    'label'    => __('add a loader element', '_ferret'),
+                    'section'  => '_ferret_theme_options_loader_section',
+                    'settings' => '_ferret_theme_options_loader_settings',
                     'type'     => 'select',
                     'choices'  => array (
                         'none'         => __(''),
@@ -143,24 +143,24 @@
             )
         );
         $wp_customize->add_setting(
-            'nishifunabashihotel_theme_options_loader_style_settings', array (
+            '_ferret_theme_options_loader_style_settings', array (
                 'default'   => 'default',
                 'transport' => 'postMessage',
             )
         );
         $style_option = array (
-            'default' => __('default', 'nishifunabashihotel'),
+            'default' => __('default', '_ferret'),
         );
         for ($i = 1; $i <= 9; $i++) {
             $style_option[$i] = __('style-' . $i);
         }
         $wp_customize->add_control(new WP_Customize_Control(
                 $wp_customize,
-                'nishifunabashihotel_loader_style',
+                '_ferret_loader_style',
                 array (
-                    'label'    => __('choice loader style', 'nishifunabashihotel'),
-                    'section'  => 'nishifunabashihotel_theme_options_loader_section',
-                    'settings' => 'nishifunabashihotel_theme_options_loader_style_settings',
+                    'label'    => __('choice loader style', '_ferret'),
+                    'section'  => '_ferret_theme_options_loader_section',
+                    'settings' => '_ferret_theme_options_loader_style_settings',
                     'type'     => 'select',
                     'choices'  => $style_option
                 )
@@ -168,14 +168,14 @@
         );
     }
     
-    function nishifunabashihotel_comment_form_change($form) {
+    function _ferret_comment_form_change($form) {
         global $user_identity, $post_id;
         $form['logged_in_as'] = '<p class="logged-in-as">' . sprintf(
             /* translators: 1: edit user link, 2: accessibility text, 3: user name, 4: logout URL */
-                __('<a href="%1$s" aria-label="%2$s" class="btn btn-primary btn-lg">Logged in as %3$s</a>&nbsp;&nbsp;<a href="%4$s" class="btn btn-info btn-lg">Log out?</a>', 'nishifunabashihotel'),
+                __('<a href="%1$s" aria-label="%2$s" class="btn btn-primary btn-lg">Logged in as %3$s</a>&nbsp;&nbsp;<a href="%4$s" class="btn btn-info btn-lg">Log out?</a>', '_ferret'),
                 get_edit_user_link(),
                 /* translators: %s: user name */
-                esc_attr(sprintf(__('Logged in as %s. Edit your profile.', 'nishifunabashihotel'), $user_identity)),
+                esc_attr(sprintf(__('Logged in as %s. Edit your profile.', '_ferret'), $user_identity)),
                 $user_identity,
                 wp_logout_url(apply_filters('the_permalink', get_permalink($post_id), $post_id))
             ) . '</p>';
@@ -183,9 +183,9 @@
         return $form;
     }
     
-    function nishifunabashihotel_logo_uploader($wp_customize) {
+    function _ferret_logo_uploader($wp_customize) {
         
-        $wp_customize->add_section('nishifunabashihotel_upload_custom_logo', array (
+        $wp_customize->add_section('_ferret_upload_custom_logo', array (
             'title'       => 'Logo',
             'description' => 'Set up a logo for your website',
             'priority'    => 25,
@@ -197,7 +197,7 @@
         
         $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'custom_logo_frontpage', array (
             'label'    => 'FrontPage Logo',
-            'section'  => 'nishifunabashihotel_upload_custom_logo', // put the name of whatever section you want to add your settings
+            'section'  => '_ferret_upload_custom_logo', // put the name of whatever section you want to add your settings
             'settings' => 'custom_logo_frontpage',
         )));
         
@@ -207,7 +207,7 @@
         
         $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'custom_logo', array (
             'label'    => 'Primary Logo',
-            'section'  => 'nishifunabashihotel_upload_custom_logo',
+            'section'  => '_ferret_upload_custom_logo',
             'settings' => 'custom_logo',
         )));
         
@@ -217,7 +217,7 @@
     
         $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'custom_logo_sp_frontpage', array (
             'label'    => 'SmartPhone Frontpage Logo',
-            'section'  => 'nishifunabashihotel_upload_custom_logo', // put the name of whatever section you want to add your settings
+            'section'  => '_ferret_upload_custom_logo', // put the name of whatever section you want to add your settings
             'settings' => 'custom_logo_sp_frontpage',
         )));
         
@@ -227,7 +227,7 @@
         
         $wp_customize->add_control(new WP_Customize_Media_Control($wp_customize, 'custom_logo_sp', array (
             'label'    => 'SmartPhone Logo',
-            'section'  => 'nishifunabashihotel_upload_custom_logo', // put the name of whatever section you want to add your settings
+            'section'  => '_ferret_upload_custom_logo', // put the name of whatever section you want to add your settings
             'settings' => 'custom_logo_sp',
         )));
         
