f15c9c86c21f69cdd6cdb0b8645a54c5c5196a92
dooioomoo <dooioomoo@gmail.com>
2020-12-29 15:43:39 +0800

    Update header.php

--- header.php
+++ header.php
@@ -25,7 +25,6 @@
 
 
 <div id="page" class="site">
-    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e('Skip to content', '_ferret'); ?></a>
     <header id="masthead" class="site-header" role="banner">
         <?php get_template_part('template-parts/header/header', 'image'); ?>
     </header><!-- #masthead -->
